0db40193723e7c091bf4c447e6da822e1ec12d84
akaya <aykutkaya940@gmail.com>
2021-12-11 18:19:08 +0300

    Work Case #1

--- src/test/java/org/example/test/LoginPageTest.java
+++ src/test/java/org/example/test/LoginPageTest.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-
+//Note
 public class LoginPageTest extends BaseTest{
     LoginPage loginPage;
 
